a727e3d94b48b501012e091558065e3daf1d0cb3
Michael Koceja and joe werderber <michael+joe@>
2017-05-11 16:13:39 -0700

    add css styling, move list in html, add img directory, make function to add players rolls together and reset if a 1 is rolled

--- js/scripts.js
+++ js/scripts.js
@@ -2,78 +2,58 @@
 //Player Constructor
 function Player() {
   this.currentRoll;
-  // this.currentRoll2;
+  this.score = 0;
 }
 
 //Player1 prototype generating random rolls 1-6
 Player.prototype.roll = function() {
   return Math.floor(Math.random()*6+1);
 }
-//Player2 prototype generatingrandom rolls 1-6
-Player.prototype.roll2 = function() {
-  return Math.floor(Math.random()*6+1);
-}
-//If Player1 roll is === 1, stop rolling, >=2 keep rolling
-Player.prototype.one = function() {
-  if (this.currentRoll === 1) {
 
-  } else if (this.currentRoll >= 2) {
-      this.currentRoll;
+Player.prototype.totalOne = function() {
+  this.score += this.currentRoll;
+    console.log(this.score);
   }
-}
-//If Player2 roll is === 1, stop rolling, >=2 keep rolling
-Player.prototype.two = function() {
-  if (this.currentRoll === 1) {
-
-  } else if (this.currentRoll >= 2) {
-      this.currentRoll;
-  }
-}
-
-// for(var index = 0; index < this.currentRoll; index++){
-//   var addPlayer1 = (this.currentRoll + "");
-//   var addTotal1 = (index);
-// }
-// console.log();
-
-// var addPlayer2 = (this.currentRoll + "");
-
 
 //User Interface Logic
 
 $(function(){
+  var player1 = new Player();
+  var player2 = new Player();
   $("#formOne").submit(function(event) {
     event.preventDefault();
 //Player1 rolled value
-    var player1 = new Player();
+
     player1.currentRoll = player1.roll();
     console.log(player1.currentRoll);
 //If rolled value is 1, empty previous rolls
     if (player1.currentRoll === 1) {
       $("#results").empty();
-    }
+      player1.score = 0;
+
 //Else continue rolling
+    }
     else {
       $("#results").show().append("<li>" + player1.currentRoll + "</li>");
+      player1.totalOne();
+      $("#score1").show(Player.totalOne); //working on adding rolls then displaying total
     }
-    $("#score1").show(addPlayer1) //working on adding rolls then displaying total
-    player1.one();
   });
 
   $("#formTwo").submit(function(event) {
       event.preventDefault();
 //Player2 rolled value
-      var player2 = new Player();
-      player2.currentRoll = player2.roll2();
+      player2.currentRoll = player2.roll();
       console.log(player2.currentRoll);
 //If rolled value is 1, empty previous rolls
       if (player2.currentRoll === 1) {
         $("#results2").empty();
+        player2.score = 0;
       }
 //Else continue rolling
       else {
         $("#results2").show().append("<li>" + player2.currentRoll + "</li>");
+        player2.totalOne();
       }
-      player2.two();
     });
 });
